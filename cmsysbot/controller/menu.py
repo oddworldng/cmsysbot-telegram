50d8155846ed925e027045da8700f5106fd4979c
Dibad <dibadgithub@gmail.com>
2019-03-27 08:58:21 +0000

    Added documentation to 'menu' module

--- cmsysbot/controller/menu.py
+++ cmsysbot/controller/menu.py
@@ -1,3 +1,23 @@
+"""
+In this module are defined all the ``menu`` :obj:`callbacks` used by the
+bot.
+
+Note:
+    A :obj:`callback` is any function that is called when pressing a button on
+    a :obj:`Keyboard`. For example, when pressing on ``Connect``, ``Wake
+    computers``, ``Disconnect``...
+
+``menu`` :obj:`callbacks` are the ones that handle the movement between
+different menus (For example, from `main menu` to `select a department`)
+
+``menu`` :obj:`callbacks` DON'T perform any other action other than handling the
+movement, and some side effects like updating :obj:`user_data` variable or
+sending some messages.
+
+Callbacks that perform an action (like waking or shutting down computers,
+updating computers/ips...) must be defined inside the :obj:`general` module.
+"""
+
 from telegram import Bot
 from telegram.ext import Updater
 
@@ -6,6 +26,16 @@ from utils import Computers, State, glob
 
 
 def new_main(bot: Bot, update: Updater, user_data: dict):
+    """
+    Send a new message with a new main menu. Depending on if the user is
+    connected or disconnected the view may change.
+
+    Args:
+        bot (:obj:`telegram.bot.Bot`): The telegram bot instance.
+        update (:obj:`telegram.ext.update.Updater`): The Updater associated to
+            the bot.
+        user_data (:obj:`dict`): The dictionary with user variables.
+    """
 
     s = user_data['session']
 
@@ -17,7 +47,16 @@ def new_main(bot: Bot, update: Updater, user_data: dict):
 
 
 def main(bot: Bot, update: Updater, user_data: dict):
-    """Show the main menu, with the most basic options for the bot"""
+    """
+    Edit the last message to show the main menu. Depending on if the user is
+    connected or disconnected the view may change.
+
+    Args:
+        bot (:obj:`telegram.bot.Bot`): The telegram bot instance.
+        update (:obj:`telegram.ext.update.Updater`): The Updater associated to
+            the bot.
+        user_data (:obj:`dict`): The dictionary with user variables.
+    """
 
     s = user_data['session']
 
@@ -29,12 +68,19 @@ def main(bot: Bot, update: Updater, user_data: dict):
 
 
 def select_department(bot: Bot, update: Updater, user_data: dict):
+    """
+    Show the nodes on the first level of the :obj:`structure` field on the
+    :obj:`config.json` file.
 
-    # TODO: Document route
+    Args:
+        bot (:obj:`telegram.bot.Bot`): The telegram bot instance.
+        update (:obj:`telegram.ext.update.Updater`): The Updater associated to
+            the bot.
+        user_data (:obj:`dict`): The dictionary with user variables.
+    """
+
     user_data['session'].route = []  # Reset route
     route = user_data['session'].route
-
-    print(route)
 
     # View
     view.structure(
@@ -43,12 +89,45 @@ def select_department(bot: Bot, update: Updater, user_data: dict):
 
 
 def structure(bot: Bot, update: Updater, user_data: dict):
+    """
+    Show the nodes on the level indicated by the route from the
+    :obj:`config.json` file.
+
+    Note:
+        The buttons shown by this function change dynamically depending on the
+        value of ``route``.
+
+        For example, if we have the following :obj:`config.json` file:
+
+        .. code-block:: python
+
+            {
+                "structure": [{
+                    "name": "ESIT",
+                    "sections": ["Industrial", "Ingeniería"]
+                },
+                {
+                    "name": "MATFIS"
+                }]
+            }
+
+        Calling this function with :obj:`remote = []` will show the buttons
+        ``ESIT`` and ``MATFIS``, but calling this function with :obj:`remote =
+        ['ESIT']` will show the buttons ``Industrial`` and ``Ingeniería``, and
+        calling this function with :obj:`remote = ['MATFIS']` will show no
+        buttons.
+
+    Args:
+        bot (:obj:`telegram.bot.Bot`): The telegram bot instance.
+        update (:obj:`telegram.ext.update.Updater`): The Updater associated to
+            the bot.
+        user_data (:obj:`dict`): The dictionary with user variables.
+    """
 
     # Get the clicked section
     next_section = update.callback_query.data
 
     route = user_data['session'].route
-    print(route)
 
     # Remove section from the route if going backwards, otherwise append
     if route and route[-1] == next_section:
